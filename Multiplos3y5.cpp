38354f1e9dce81c830670dc3721d32ad2f401685
JosePeredaC <63888647+JosePeredaC@users.noreply.github.com>
2024-03-26 11:10:54 -0500

    Actualizar Multiplos3y5.cpp

--- Multiplos3y5.cpp
+++ Multiplos3y5.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-    //Declaración de variables
+    //DeclaraciÃ³n de variables
     int inicial = 0;
     int suma = 0;
     //Funcion principal: Mientras "inicial sea menor que 1000" se ejecuta el codigo interior
     while (inicial < 1000){
         cout<<inicial;
-        //Busca los valores multiplos de 3 y 5
+        //Busca los valores multiplos de 3 o 5
         if (inicial % 3 == 0){
             suma = suma + inicial;
             cout<<endl;
